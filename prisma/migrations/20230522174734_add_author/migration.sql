17da33c794e2c3af49d8e1214e8a2c7f36c6f7ed
makbulut42 <makbulut@student.42istanbul.com.tr>
2023-05-23 10:16:51 +0300

    [change, delete]

--- prisma/migrations/20230522174734_add_author/migration.sql
+++ prisma/migrations/20230522174734_add_author/migration.sql
@@ -4,16 +4,10 @@ CREATE TABLE "Author" (
     "name" TEXT NOT NULL
 );
 
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_Books" (
+-- CreateTable
+CREATE TABLE "Books" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
     "authorId" INTEGER,
     CONSTRAINT "Books_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
-INSERT INTO "new_Books" ("id", "title") SELECT "id", "title" FROM "Books";
-DROP TABLE "Books";
-ALTER TABLE "new_Books" RENAME TO "Books";
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
